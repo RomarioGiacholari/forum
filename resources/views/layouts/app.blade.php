aac163135476010af91eaae028ef1d1895637788
Romario Giacholari <exit>
2017-04-21 19:21:15 +0300

    Fixed caret

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,12 +79,6 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
@@ -96,8 +90,6 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
-                                </ul>
-                            </li>
                         @endif
                           </ul>
                          </li>
