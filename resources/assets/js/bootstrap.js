8316534c8199dd120d815ff42362d06f6b9d7585
Romario Giacholari <exit>
2017-05-23 00:36:34 +0100

    minor changes

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -25,7 +25,7 @@ require('bootstrap-sass');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-//window.axios = require('axios');
+window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
