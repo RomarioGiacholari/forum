fdab9ce6d9778f481b3e910d824ae44673a68552
Romario Giacholari <exit>
2017-05-22 22:36:12 +0100

    spelling mistake

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -30,7 +30,7 @@
             </div>
         </div>
         @empty
-        <blockquote>There are no post yet listed for this category</blockquote>
+        <blockquote>There are no posts yet listed for this category.</blockquote>
         @endforelse
 
        <!-- <a href="/threads/create" class="btn btn-default" role="button" style =' position: fixed;bottom: 0;right: 0;width:100%;font-size:1em;opacity:0.9;color:black'>Post a Question</a> -->
