8f95ec6606cfede84072a8a50e44659509c82cb1
Romario Giacholari <exit>
2017-06-16 17:30:58 +0100

    Pntrest layout -- threads.index

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,10 +4,9 @@
 
 <div class="container">
     <div class="row">
+    <section id="pinBoot">
     @forelse($threads as $thread)
-        <div class="col-md-4 col-md-offset-0">
-            <div class="panel panel-default" style="overflow:hidden;height:250px;">
-                <div class="panel-heading">
+            <div class="white-panel"">
                 <div class ='level'>
                  <div class='flex'>
                   <h4><strong><a style='color:black' href = "{{$thread->path()}}">{{$thread->title}}</a></strong></h4>
@@ -18,16 +17,16 @@
                  {{$thread->replies_count}} <i class="fa fa-reply" aria-hidden="true"></i>
                  </a>
                 </div>
-                </div>
                 <div class="panel-body">
                    <p id="thread-text">{{$thread->body}}</p>
                 </div>
             </div>
-        </div>
         @empty
         <blockquote>There are no posts yet listed for this category.</blockquote>
         @endforelse
+       </section>
 
        <!-- <a href="/threads/create" class="btn btn-default" role="button" style =' position: fixed;bottom: 0;right: 0;width:100%;font-size:1em;opacity:0.9;color:black'>Post a Question</a> -->
+  </div>
 </div>
 @endsection
