adcc8b17381e8b06b83c993c152c6bdf0255176c
Romario Giacholari <giacholari@gmail.com>
2017-10-06 19:37:13 +0100

    edited modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,10 +71,6 @@
 </div>
 
         @yield('content') 
-
-          <!-- Button trigger modal -->
-
-
   <!-- Modal -->
   <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
     <div class="modal-dialog" role="document">
