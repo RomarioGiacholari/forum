81b44cc0b2e296d87aa538776b4336cd9e0c3ac1
RomarioGiacholari <giacholari@gmail.com>
2017-12-11 19:26:49 +0000

    relocated manifest.json to public directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .json('resources/assets/manifest.json', 'public/');
+   .sass('resources/assets/sass/app.scss', 'public/css');
